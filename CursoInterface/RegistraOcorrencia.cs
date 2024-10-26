2fb6621b9afd185c72c1c8da7f91b1714efa7ad6
Vinicius Silva <vinyoli.vs@gmail.com>
2024-10-26 09:43:59 -0300

    Initial commit

---
+++ CursoInterface/RegistraOcorrencia.cs
@@ -0,0 +1,25 @@
+﻿using CursoInterface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoInterface
+{
+    public class RegistraOcorrencia : IRegistro
+    {
+
+        private readonly IRegistro _registro;   
+
+        public RegistraOcorrencia(IRegistro registro)
+        {
+            _registro = registro;
+        }
+        
+        public void RegistraInfo(string mensagem)
+        {
+            _registro.RegistraInfo($"{mensagem} : {DateTime.Now}");
+        }
+    }
+}
---
+++ CursoInterface/RegistrarNoArquivo.cs
@@ -0,0 +1,35 @@
+﻿using CursoInterface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoInterface
+{
+    internal class RegistrarNoArquivo : IRegistro
+    {
+        private readonly string _caminhoNomeArquivo;
+
+        public RegistrarNoArquivo(string path)
+        {
+            _caminhoNomeArquivo = path;
+        }
+
+        public void RegistraInfo(string mensagem)
+        {
+            Log(mensagem);
+        }
+
+        private void Log(string mensagem)
+        {
+            using(var streamWriter = new StreamWriter(_caminhoNomeArquivo, true))
+            {
+                streamWriter.WriteLine(mensagem);
+            }
+        }
+                
+       
+    }
+}
